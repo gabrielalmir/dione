ba25e40fa1300989ca07291a311cd370b802a253
gabrielalmir <gabr.almir@gmail.com>
2024-09-16 20:35:32 -0300

    feat: add configurable port (default to 4321)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,7 +9,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 
 bootstrap();
