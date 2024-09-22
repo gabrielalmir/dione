17b331d6a22710bdc39db6f4963ae815391cc5c7
gabrielalmir <gabr.almir@gmail.com>
2024-09-22 09:42:03 -0300

    feat: increase swagger description

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Dione')
-    .setDescription('Dione is an HTTP-based task queuing system designed for environments with limited infrastructure.')
+    .setDescription('Dione is an HTTP-based task queuing system designed for environments with limited infrastructure. Developed in TypeScript, Node.js & NestJS, Dione is ideal for situations where a simple yet effective queuing solution is needed without the complexity or additional infrastructure overhead.')
     .setVersion('0.0.1')
     .addBearerAuth()
     .build();
